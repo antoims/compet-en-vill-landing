c12073b82988350c2051b8e3afaa1ec186779651
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-09-28 21:19:36 +0200

    notif when prod

--- app/controllers/registrants_controller.rb
+++ app/controllers/registrants_controller.rb
@@ -5,8 +5,10 @@ class RegistrantsController < ApplicationController
     @registrant = Registrant.new(registrant_params)
 
     if @registrant.save
-      SlackNotificationService.new(@registrant).call
-      RegistrantNotifierMailer.send_welcome_email(@registrant).deliver_later
+      if Rails.env.production?
+        SlackNotificationService.new(@registrant).call
+        RegistrantNotifierMailer.send_welcome_email(@registrant).deliver_later
+      end
       render json: {
         status: true,
         popup: render_to_string(partial: "registrant_popup", locals: { registrant: @registrant }),
