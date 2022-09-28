4d51171f7a97a15f05b4c30a953169e4dbb2d414
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-09-28 18:04:23 +0200

    comment slack and mailer notif when test design

--- app/controllers/registrants_controller.rb
+++ app/controllers/registrants_controller.rb
@@ -4,8 +4,8 @@ class RegistrantsController < ApplicationController
     @registrant = Registrant.new(registrant_params)
 
     if @registrant.save
-      SlackNotificationService.new(@registrant).call
-      RegistrantNotifierMailer.send_welcome_email(@registrant).deliver
+      # SlackNotificationService.new(@registrant).call
+      # RegistrantNotifierMailer.send_welcome_email(@registrant).deliver
       render json: {
         status: true,
         popup: render_to_string(partial: "registrant_popup", locals: { registrant: @registrant }),
