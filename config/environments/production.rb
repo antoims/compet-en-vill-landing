435e58bffe11fc9b4ee324772663122bf918dc67
Antoine Gros <antoine.gros14@gmail.com>
2022-10-06 18:14:42 +0200

    dont presice https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'https://compet-en-vill-landing.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'compet-en-vill-landing.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
