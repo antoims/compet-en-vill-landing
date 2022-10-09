8546d450530fff57f3dd601021ee063874827009
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-10-09 19:47:36 +0200

    case insensitive email

--- app/models/registrant.rb
+++ app/models/registrant.rb
@@ -1,6 +1,6 @@
 class Registrant < ApplicationRecord
   validates :email,
     presence: true,
-    uniqueness: true,
+    uniqueness: { case_sensitive: false },
     format: { with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i, multiline: true, message: "L'adresse mail n'est pas valide" }
 end
